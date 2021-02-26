0d3ba05f5ad77bbc33fdea11665f646353055ee4
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-02-26 23:00:03 +0100

    chore: tests

--- src/parseLog.js
+++ src/parseLog.js
@@ -3,7 +3,19 @@ const path = require("path");
 
 const parseLog = (filename) => {
   const logData = fs.readFileSync(filename).toString();
-  const entries = logData.split("\n").map(JSON.parse);
+  const entries = logData
+    .split("\n")
+    .filter(Boolean)
+    .map((line) => {
+      let data;
+      try {
+        data = JSON.parse(line);
+      } catch (e) {
+        console.log("e", e);
+      }
+      return data;
+    })
+    .filter(Boolean);
   return entries;
 };
 
